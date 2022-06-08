1ef484aa5f3e1aa619865cdaec009ca39f2821f3
211128 <84791442+211128@users.noreply.github.com>
2022-06-08 11:53:43 -0500

    Chante to ES6

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -1,18 +1,17 @@
 import { Router } from 'express';
-import { success} from '../../../network/response.js';
-import {getData} from '../../../model/db.js'
-import { getUser } from "../../../model/User.js";
-
-
+import { success} from '../../network/response.js';
+import getConnection from "../../model/db.js";
 
 
 const router = Router();
 
+router.get('/s',function (req, res) {
+    /*res.send({
+        success:"succes 1",
+    });*/
+    succes(req, res ,'', 200);
+})
 
-router.get('/success', function (req, res) {
-    success(req, res, "", 200);
-
-});
 router.post('/login', function (req, res) {
 
     let userName = req.query.userName;
@@ -22,13 +21,11 @@ router.post('/login', function (req, res) {
         token: "",
         id_user: "1",
         success: "exito",
-    });
-});
-
+    })
+})
 
 router.post('/register', async function (req, res) {
- 
-    console.log('register');
+
     const client = await getConnection();
 
     let username = req.query.username;
@@ -36,68 +33,65 @@ router.post('/register', async function (req, res) {
     let password = req.query.password;
     let phone_number = req.query.phone_number;
 
-    console.log(username, email, password, phone_number)
-
-    const query_request = {
-        text: 'INSERT INTO tbl_usersdb(username, email, password, phone_number) VALUES ($1, $2, $3, $4)',
+    const query_request ={
+        text: 'INSERT INTO tbl_usersdb(username, email, password, phone_number) VALUES($1, $2, $3, $4)',
         values: [username, email, password, phone_number]
     };
 
-    client.query(query_request)
-        .then(r => { console.log('1'); success(req, res, r, 200); })
-        .catch(e => { console.log('2'); success(req, res, e.stack, 200); })
+    (await client).query(query_request)
+    .then(r => {success(req,res,r,200);})
+    .catch(e => {success(req,res,e.detail,200);})
 
-});
-
-router.delete('/delete', async function (req, res) {
+})
+router.get('/mostrar', async function (req, res) {
 
     const client = await getConnection();
 
-    let idUser = req.query.id;
-
-    const query_request = {
-        text: `delete from tbl_usersdb where id = ${idUser}`
+    const query_request ={
+        text: 'SELECT * FROM public.tbl_usersdb'
     };
 
-    client.query(query_request)
-        .then(r => { console.log('1'); _success(req, res, r, 200); })
-        .catch(e => { console.log('2'); _success(req, res, e.stack, 200); })
-});
+    (await client).query(query_request)
+    .then(r => {success(req,res,r,200);})
+    .catch(e => {success(req,res,e.detail,200);})
 
-router.put('/update', async function (req, res) {
-   
+})
+
+router.delete('/eliminar', async function (req, res) {
+
     const client = await getConnection();
 
-    let idUser = req.query.id;
+    let id = req.query.id;
+
+    const query_request ={
+        text: `DELETE FROM tbl_usersdb WHERE id=${id}`
+    };
+
+    (await client).query(query_request)
+    .then(r => {success(req,res,r,200);})
+    .catch(e => {success(req,res,e.detail,200);})
+
+})
+
+router.put('/actualizar', async function (req, res) {
+
+    const client = await getConnection();
+
+    let id = req.query.id;
     let username = req.query.username;
     let email = req.query.email;
     let password = req.query.password;
     let phone_number = req.query.phone_number;
 
-    const query_request = {
-        text: `UPDATE  tbl_usersdb SET username= '${username}', email= '${email}', password= '${password}', phone_number= '${phone_number}' WHERE id= '${idUser}'`
+    const query_request ={
+        text: `UPDATE tbl_usersdb SET username = '${username}', email = '${email}' , password = '${password}' , phone_number = '${phone_number}' where id = '${id}'`
     };
 
-    client.query(query_request)
-        .then(r => { console.log('1'); success(req, res, r, 200); })
-        .catch(e => { console.log('2'); success(req, res, e.stack, 200); })
-});
+    (await client).query(query_request)
+    .then(r => {success(req,res,r,200);})
+    .catch(e => {success(req,res,e.detail,200);})
 
-router.get('/list', async function (req, res) {
-    
-    const client = await getConnection();
-
-    const query_request = {
-        text: 'select * from tbl_usersdb'
-    };
-    client.query(query_request, (err,result) => {
-        res.send(result.rows)
-    })
-       
-});
-
-
-
+})
 
 
 export default router;
--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
- import express from 'express';
- import { api } from '../config.js';
- import user from './components/user/network.js';
+import express from 'express';
 
-const app = express();  
+import { api } from '../config.js';
+import user from '../components/user/network.js';
+
+const app = express();
 
 //ROUTERS
- app.use('/api/user', user);
-//  app.use(cors({origin: true, credential: true}));
+app.use('/api/user', user)
 
 //Servidor activo
-app.listen( api.port ,() => {
-    console.log('Servidor corriendo en el puerto en el puerto =>',"3000")
+app.listen(api.port, () => {
+    console.log('Servidor corriendo en el puerto en el puerto =>', api.port)
 }
 );
-
