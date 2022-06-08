b535e86a33bacfbc1706ebde4b7d72a3281be9b1
211128 <84791442+211128@users.noreply.github.com>
2022-06-08 11:56:12 -0500

    Add ORM sequalize

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
-import express from 'express';
+ import express from 'express';
+ import { api } from '../config.js';
+ import user from './components/user/network.js';
 
-import { api } from '../config.js';
-import user from '../components/user/network.js';
-
-const app = express();
+const app = express();  
 
 //ROUTERS
-app.use('/api/user', user)
+ app.use('/api/user', user);
+//  app.use(cors({origin: true, credential: true}));
 
 //Servidor activo
-app.listen(api.port, () => {
-    console.log('Servidor corriendo en el puerto en el puerto =>', api.port)
+app.listen( api.port ,() => {
+    console.log('Servidor corriendo en el puerto en el puerto =>',"3000")
 }
 );
+
--- model/db.js
+++ model/db.js
@@ -1,19 +1,32 @@
-import pkg from 'pg';
-//const { getConnection } = require('');
+// existe mas de una clase cuando se encuentra entre llaves {}
+import pkg from 'pg'
 const {Pool} = pkg;
+import { Sequelize } from 'sequelize';
 import { db } from '../config.js';
 
-
-async function getConnection(){
+async function getConnection() {
     const client = new Pool({
         user: db.user,
         host: db.host,
         database: db.database,
         password: db.password,
-        port: db.port
+        port: db.port,
     });
     await client.connect();
     return client;
-}
 
-export default getConnection;
+}
+const sequelizeClient = new Sequelize(db.database, db.user, db.password,{
+    host: db.host,
+    dialect: 'postgres',
+});
+
+sequelizeClient.authenticate()
+    .then(() => {
+        console.log('Conectado')
+    })
+    .catch(() => {
+        console.log('no se conecto')
+    })
+    ;
+export  const getData = {getConnection, sequelizeClient} ;
