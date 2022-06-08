84a33d21d69baff572a972a1f135d7254980a71c
211128 <84791442+211128@users.noreply.github.com>
2022-06-08 11:49:51 -0500

    Changes to ES6

---
+++ back end/model/db.js
@@ -0,0 +1,19 @@
+import pkg from 'pg';
+//const { getConnection } = require('');
+const {Pool} = pkg;
+import { db } from '../config.js';
+
+
+async function getConnection(){
+    const client = new Pool({
+        user: db.user,
+        host: db.host,
+        database: db.database,
+        password: db.password,
+        port: db.port
+    });
+    await client.connect();
+    return client;
+}
+
+export default getConnection;
