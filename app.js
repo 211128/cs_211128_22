b7ea33f611cb1661432059e11fef700b59cc3eeb
211128 <2111128@ids.upchiapas.edu.mx>
2022-06-14 08:52:34 -0500

    changes project

---
+++ app.js
@@ -0,0 +1,16 @@
+ import express from 'express';
+ import { api } from './config.js';
+ import user from './api/components/user/network.js';
+
+const app = express();  
+
+//ROUTERS
+ app.use('/api/user', user);
+//  app.use(cors({origin: true, credential: true}));
+
+//Servidor activo
+app.listen( api.port ,() => {
+    console.log('Servidor corriendo en el puerto en el puerto =>',"3000")
+}
+);
+
