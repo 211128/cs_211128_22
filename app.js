21f852f78200cc3786641b7fa3da0478c7349571
211128 <211128@ids.upchiapas.edu.mx>
2022-06-24 19:51:20 -0500

    Changes in app

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
  import express from 'express';
  import { api } from './config/config.js';
  import user from './router/user.js';
+ import father from './router/father.js';
+ import image from './router/image.js';
+ import son from './router/son.js';
+ 
 
 const app = express();  
 
 //ROUTERS
  app.use('/api/user', user);
+ app.use('/api/father', father);
+ app.use('/api/image', image);
+ app.use('/api/son', son);
+
 //  app.use(cors({origin: true, credential: true}));
 
 //Servidor activo
